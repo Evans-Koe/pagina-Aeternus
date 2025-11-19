825de5f36335db507f9454de6f92ce16fd38f06f
Evans-Koe <evanssierra72@gmail.com>
2025-11-19 14:03:23 -0400

    Add files via upload

---
+++ config_db.py
@@ -0,0 +1,7 @@
+DB_CONFIG = {
+    "host": "localhost",
+    "user": "root",
+    "password": "123321123mMm",
+    "database": "AeternusDB",
+    "port": 3306
+}
---
+++ config_email.py
@@ -0,0 +1,4 @@
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587
+EMAIL_USER = "aeternus.acc@gmail.com"
+EMAIL_PASS = "smqz txfi vuub cwax"
